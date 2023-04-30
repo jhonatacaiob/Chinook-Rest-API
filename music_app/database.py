b152fd3e6d43e17b0591bc961a8dac873b2124f6
jhonatacaiob <jhonatacaio@gmail.com>
2023-04-30 01:32:53 -0300

    corrige chamada ao banco
    Agora, utiliza-se .env para montar a string de conexao

--- music_app/database.py
+++ music_app/database.py
@@ -1,8 +1,24 @@
-from flask_sqlalchemy import SQLAlchemy
+import os
 
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.engine import URL
+from dotenv import load_dotenv
+
+
+load_dotenv()
 db = SQLAlchemy()
 
 
 def configure(app):
+    url = URL.create(
+        os.getenv('DRIVERNAME'),
+        username=os.getenv('USERNAME'),
+        password=os.getenv('PASSWORD'),
+        host=os.getenv('HOST'),
+        database=os.getenv('DATABASE'),
+        port=os.getenv('PORT'),
+    )
+
+    app.config['SQLALCHEMY_DATABASE_URI'] = url
     app.db = db
     db.init_app(app)
