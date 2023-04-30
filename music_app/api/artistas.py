d3016c9216b1e48948b01d8f927b32754d48bc94
jhonatacaiob <jhonatacaio@gmail.com>
2023-04-30 17:44:10 -0300

    Cria CRUD de artistas

--- music_app/api/artistas.py
+++ music_app/api/artistas.py
@@ -12,28 +12,77 @@ bp = Blueprint('artistas', __name__, url_prefix='/artistas')
 
 @bp.get('/')
 def listar():
-    db = current_app.db
-    artistas = db.session.execute(db.select(Artista)).fetchall()
+    query = current_app.db.select(Artista)
+    artistas = current_app.db.session.execute(query).fetchall()
+    return [asdict(artista[0]) for artista in artistas], 200
 
-    return [asdict(artista[0]) for artista in artistas]
 
 @bp.get('/<id>')
 def visualizar(id):
-    db = current_app.db
-    artista = db.get_or_404(Artista, id)
-    
-    return asdict(artista)
+    artista = current_app.db.get_or_404(Artista, id)
+
+    return asdict(artista), 200
 
 
-@bp.post('/<id>')
-def criar(id):
-#    data = request.get_json()
-    ...
+@bp.post('/')
+def criar():
+    if request.is_json:
+        try:
+            payload = request.get_json()
+            artista = Artista(**payload)
+
+            if artista.id is not None or artista.nome is None:
+                raise TypeError()
+
+            stmt = current_app.db.session.add(artista)
+            current_app.db.session.commit()
+
+            return (
+                {
+                    'artista': asdict(artista),
+                    'url': f'{request.base_url}{artista.id}',
+                    'mensagem': f'O artista de   nome {artista.nome} foi criado com sucesso',
+                },
+                201,
+            )
+
+        except TypeError:
+            return {'erro': 'Verifique os campos enviados na requisição'}, 400
+
+    return {'erro': 'O payload precisa ser um json'}, 400
+
 
 @bp.put('/<id>')
 def editar(id):
-    ...
+    if request.is_json:
+        try:
+            payload = request.get_json()
+            artista_payload = Artista(**payload)
+
+            if artista_payload.id is not None or artista_payload.nome is None:
+                raise TypeError()
+
+            artista_atual = current_app.db.get_or_404(Artista, id)
+            artista_atual.nome = artista_payload.nome
+
+            current_app.db.session.commit()
+            return (
+                {
+                    'artista': asdict(artista_atual),
+                    'url': f'{request.base_url}{artista_atual.id}',
+                },
+                200,
+            )
+        except TypeError:
+            return {'erro': 'Verifique os campos enviados na requisição'}, 400
+
+    return {'erro': 'O payload precisa ser um json'}, 400
+
 
 @bp.delete('/<id>')
 def deletar(id):
-    ...
+    artista_atual = current_app.db.get_or_404(Artista, id)
+    current_app.db.session.delete(artista_atual)
+    current_app.db.session.commit()
+
+    return {}, 204
--- music_app/models/artista.py
+++ music_app/models/artista.py
@@ -2,23 +2,14 @@ from dataclasses import dataclass
 
 from flask import current_app
 from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy.orm import synonym
-
 
 db = SQLAlchemy()
 
 
 @dataclass
 class Artista(db.Model):
-    __tablename__ = 'Artist'
 
-    id: db.Mapped[int]
-    name: db.Mapped[str]
-
-    ArtistId = db.Column(db.Integer, primary_key=True)
-    Name = db.Column(
-        db.String,
+    id: db.Mapped[int] = db.mapped_column(
+        primary_key=True,
     )
-
-    id = synonym('ArtistId')
-    name = synonym('Name')
+    nome: db.Mapped[str] = db.mapped_column()
