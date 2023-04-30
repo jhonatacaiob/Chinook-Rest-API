f613a76302c461cce0bfb1c9407f6c29a322c4e1
jhonatacaiob <jhonatacaio@gmail.com>
2023-04-30 00:13:01 -0300

    Configura banco de dados

--- music_app/__init__.py
+++ music_app/__init__.py
@@ -1,12 +1,19 @@
 import os
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 
 from .api import music
+from .database import configure
 
 
 def create_app(test_config=None):
     # create and configure the app
     app = Flask(__name__, instance_relative_config=True)
+
+    app.config['SQLALCHEMY_DATABASE_URI'] = os.environ[
+        'SQLALCHEMY_DATABASE_URI'
+    ]
+    configure(app)
 
     if test_config is None:
         # load the instance config, if it exists, when not testing
